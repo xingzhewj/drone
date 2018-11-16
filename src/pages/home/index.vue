386d3d580549a355ae4ac26e1189280f973c1a89
wangjie19 <wangjie19@baidu.com>
2018-11-16 21:56:48 +0800

    create-drone

---
+++ src/pages/home/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home-container">
+        <p>home</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.home-container {
+    color: #f00;
+    p {
+        font-size: 30px;
+    }
+}
+</style>
+
